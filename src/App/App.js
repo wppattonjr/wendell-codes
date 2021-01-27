b12410714b58ae54a8002214efa7f2a60e760b6f
Wendell <wendell.pattonjr@gmail.com>
2021-01-27 00:09:10 -0600

    setup react personal site

---
+++ src/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>WENDELL CODES</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
